b189d17610f0e0cbefb36e558d0af3a2f71ff7e7
kazuma mitsuyoshi <k-mitsuyoshi@leapy.jp>
2024-10-26 22:49:15 +0900

    outputをserverに変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server', // または 'hybrid'
+  // ... その他の設定 ...
+});
