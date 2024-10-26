9be19865d7851fda01d32e0658a7e1a4b7452386
kazuma mitsuyoshi <k-mitsuyoshi@leapy.jp>
2024-10-26 21:56:20 +0900

    scssの導入と、コンポーネント化

--- src/lib/newt.ts
+++ src/lib/newt.ts
@@ -3,7 +3,17 @@ import { createClient } from 'newt-client-js'
 export interface Article {
   title: string
   slug: string
+  meta: string
   body: string
+}
+export interface Tag {
+  name: string
+  slug: string
+}
+export interface Navigation {
+  nav_text: string
+  header_logo: string
+  header_nav: string
 }
 
 export const newtClient = createClient({
