fde483b522a09289583b7be43028a569a0db622b
kazuma mitsuyoshi <k-mitsuyoshi@leapy.jp>
2024-11-01 07:03:58 +0900

    add: SSRアダプターを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless'; // Vercelアダプターをインポート
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server', // または 'hybrid'
+  adapter: vercel(), // Vercelアダプターを追加
 });
